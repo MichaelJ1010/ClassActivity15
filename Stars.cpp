9e54e788cba26052b5a30ccb6981321c1bb5b9a1
Michael J <30031129@cc.peralta.edu>
2024-09-23 11:02:09 -0700

    added nested for loop

--- Stars.cpp
+++ Stars.cpp
@@ -7,5 +7,12 @@ int main(){
     int numberOfRows;
     cout << "how many rows of stars do you want?" << endl;
     cin >> numberOfRows;
+
+    for(int i = 0; i < numberOfRows; i++){
+        for(int j = 0; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
     
 }
